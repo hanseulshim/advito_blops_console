a2d2c0907ae2ec9abc2c6e23e44ab568de9fdfff
Hanseul Shim <hanseulshim@gmail.com>
2018-11-01 12:35:25 -0400

    Add password to app

--- src/App.js
+++ src/App.js
@@ -2,13 +2,32 @@ import React, { Component } from 'react';
 import './App.scss';
 import Content from './components/Content';
 import Sidebar from './components/Sidebar';
+import Password from './components/Password';
 
 class App extends Component {
+  state = {
+    passwordValid: false,
+  }
+  checkPassword = (password) => {
+    const passwordValid = password === 'blops2018';
+    this.setState({ passwordValid });
+  }
   render() {
+    const passwordValid = this.state.passwordValid;
     return (
       <div id="app">
-        <Sidebar />
-        <Content />
+        {
+          passwordValid ?
+          <>
+            <Sidebar />
+            <Content />
+          </> :
+          <Password checkPassword={this.checkPassword} />
+        }
+        {/* <>
+          <Sidebar />
+          <Content />
+        </> */}
       </div>
     );
   }
