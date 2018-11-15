d4dbdd2e96c75ce7bde48832ddff952b0373ee9b
Hanseul Shim <hanseulshim@gmail.com>
2018-11-15 17:05:50 -0500

    Update Hingle to Shayan

--- src/components/Sidebar/SidebarUserInfo.js
+++ src/components/Sidebar/SidebarUserInfo.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import WalrusImage from 'assets/walrus.jpeg';
+import Shayan from 'assets/shayan.jpeg';
 
 const SidebarUserInfo = () => {
   return (
     <div className="sidebar-user-info">
-      <img src={WalrusImage} alt="avatar"/>
+      <img src={Shayan} alt="avatar"/>
       <div>
-        <span>Hingle McCringleberry</span>
+        <span>Shayan Kheradmand</span>
         <i className="icon fas fa-cog"></i>
       </div>
     </div>
