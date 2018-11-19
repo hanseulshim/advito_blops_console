60ffe5a8049e4211af764242516d9f837b62db93
Hanseul Shim <hanseulshim@Hanseuls-MacBook-Pro.local>
2018-11-19 10:11:45 -0500

    Add portal view

--- src/App.js
+++ src/App.js
@@ -1,33 +1,29 @@
 import React, { Component } from 'react';
 import './App.scss';
-import Content from './components/Content';
-import Sidebar from './components/Sidebar';
-import Password from './components/Password';
+// import Password from './components/Password';
+import Portal from './components/Portal';
 
 class App extends Component {
   state = {
     passwordValid: false,
-  }
-  checkPassword = (password) => {
+  };
+  checkPassword = password => {
     const passwordValid = password === 'blops2018';
     this.setState({ passwordValid });
-  }
+  };
   render() {
-    const passwordValid = this.state.passwordValid;
+    // const passwordValid = this.state.passwordValid;
     return (
       <div id="app">
-        {
+        {/* {
           passwordValid ?
           <>
             <Sidebar />
             <Content />
           </> :
           <Password checkPassword={this.checkPassword} />
-        }
-        {/* <>
-          <Sidebar />
-          <Content />
-        </> */}
+        } */}
+        <Portal />
       </div>
     );
   }
