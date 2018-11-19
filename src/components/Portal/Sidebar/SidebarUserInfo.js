d92ba2e603564546e75e33371a6351a6ba20faa0
Hanseul Shim <hanseulshim@gmail.com>
2018-11-19 10:11:45 -0500

    Add portal view

--- src/components/Portal/Sidebar/SidebarUserInfo.js
+++ src/components/Portal/Sidebar/SidebarUserInfo.js
@@ -1,17 +1,16 @@
 import React from 'react';
-import WalrusImage from 'assets/walrus.jpeg';
 import Shayan from 'assets/shayan.jpeg';
 
 const SidebarUserInfo = () => {
   return (
     <div className="sidebar-user-info">
-      <img src={Shayan} alt="avatar"/>
+      <img src={Shayan} alt="avatar" />
       <div>
         <span>Shayan Kheradmand</span>
-        <i className="icon fas fa-cog"></i>
+        <i className="icon fas fa-cog" />
       </div>
     </div>
   );
-}
+};
 
 export default SidebarUserInfo;
---
+++ src/components/Portal/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Content from './Content';
+import Sidebar from './Sidebar';
+
+const Portal = () => {
+  return (
+    <>
+      <Sidebar />
+      <Content />
+    </>
+  );
+};
+
+export default Portal;
