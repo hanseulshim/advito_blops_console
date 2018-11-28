8180a6a08cd0b2c65c31fbfb68ddd0df199fa6c4
John O'Neil <joneil@boostlabs.com>
2018-11-28 12:40:17 -0500

    App.js Basic Routing & Link to dashboard from Portal

--- src/components/Dashboard/Header.js
+++ src/components/Dashboard/Header.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import BackButton from 'components/common/BackButton';
 import advito_analytics from 'assets/advito_analytics.png';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <div className="header">
-      <BackButton />
+      <Link to="/"><BackButton /></Link>
       <img className="header-logo" src={advito_analytics} alt="advito analytics" />
     </div>
   );
