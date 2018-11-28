b5f7db3f7c562ae4c08c2103dcd1b1b56cdd54ae
John O'Neil <joneil@boostlabs.com>
2018-11-28 13:51:54 -0500

    Dashboard chart containers added.

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './Header';
 import './Dashboard.scss';
+import SummaryChartRow from './SummaryChartRow';
 
 const Dashboard = () => {
   return (
     <div className="dashboard-container">
       <Header />
+      <SummaryChartRow/>
     </div>
   );
 };
---
+++ src/components/Dashboard/SummaryChartRow/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './SummaryChartRow.scss';
+
+const SummaryChartRow = () => {
+    return (
+        <div className='chartRow'>
+            <div>
+                <h1>Viz1</h1>
+            </div>
+            <div>
+                <h1>Viz2</h1>
+            </div>
+        </div>
+    );
+}
+
+export default SummaryChartRow;
