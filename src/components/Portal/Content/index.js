49e7aa78a8479e60b7f2f5b6d3551cb89677c750
Hanseul Shim <hanseulshim@gmail.com>
2018-11-28 20:05:26 -0500

    Style back button

--- src/components/Portal/Content/index.js
+++ src/components/Portal/Content/index.js
@@ -1,17 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
 import './Content.scss';
 import Header from './Header';
 import ContentMain from './ContentMain';
 
+//TODO: eventually delete
 import { viewData, infoData } from 'data/viewData';
+
+const Container = styled.div`
+  flex: 1;
+  background: $alabaster;
+  border: 1px solid $pumice;
+  padding: 4em 2em;
+`;
 
 const Content = () => {
   return (
-    <div className="content-container">
+    <Container className="content-container">
       <Header />
       <ContentMain viewData={viewData} infoData={infoData} />
-    </div>
+    </Container>
   );
-}
+};
 
 export default Content;
