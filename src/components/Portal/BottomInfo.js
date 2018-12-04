f514d980c4f723061ae7803ba9f04bcff21c723c
Hanseul Shim <hanseulshim@gmail.com>
2018-12-04 15:26:27 -0500

    Add graphql to apps to pull from api

--- src/components/Portal/BottomInfo.js
+++ src/components/Portal/BottomInfo.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
 import Icon from 'components/common/Icon';
 import Button from 'components/common/Button';
 
@@ -29,15 +31,32 @@ const Title = styled.span`
   margin-bottom: 0.75em;
 `;
 
-const generateInfoViews = data =>
-  data.map((view, index) => (
-    <Info key={index}>
-      <InfoIcon className={view.icon} />
-      <Text>
-        <Title>{view.title}</Title>
-        <Button text={view.button} />
-      </Text>
-    </Info>
-  ));
-
-export default ({ data }) => <Container>{generateInfoViews(data)}</Container>;
+export default () => (
+  <Container>
+    <Query
+      query={gql`
+        {
+          infoList {
+            title
+            icon
+            button
+          }
+        }
+      `}
+    >
+      {({ loading, error, data }) => {
+        if (loading) return <p>Loading...</p>;
+        if (error) return <p>Error :(</p>;
+        return data.infoList.map((view, index) => (
+          <Info key={index}>
+            <InfoIcon className={view.icon} />
+            <Text>
+              <Title>{view.title}</Title>
+              <Button text={view.button} />
+            </Text>
+          </Info>
+        ));
+      }}
+    </Query>
+  </Container>
+);
