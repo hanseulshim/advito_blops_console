e3afdcd1c3e11a74a4bb8b2199944a192941c22c
Hanseul Shim <hanseulshim@Hanseuls-MacBook-Pro.local>
2018-12-08 11:42:23 -0500

    Add Program Performance

--- src/components/common/Icon.js
+++ src/components/common/Icon.js
@@ -1,7 +1,14 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const IconStyled = styled.i``;
+const IconStyled = styled.i`
+  color: ${props => props.info && props.theme.jaffa};
+  border: ${props => props.info && `2px solid ${props.theme.jaffa}`};
+  padding: ${props => props.info && '0.25em .6em'};
+  border-radius: ${props => props.info && '100%'};
+  font-size: ${props => props.info && '80%'};
+  margin-left: ${props => props.info && '1em'};
+`;
 
 const Icon = props => {
   const { className, ...style } = props;
