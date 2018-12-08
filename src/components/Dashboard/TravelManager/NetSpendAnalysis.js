7c5beed88d6ae30f55a22c2562135228657b0cc4
Hanseul Shim <hanseulshim@Hanseuls-MacBook-Pro.local>
2018-12-08 11:51:32 -0500

    Set up netspendanalysis

---
+++ src/components/Dashboard/TravelManager/NetSpendAnalysis.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import styled from 'styled-components';
+import Icon from 'components/common/Icon';
+
+const Container = styled.div`
+  flex: 1;
+  padding-left: 2em;
+`;
+
+const SectionTitle = styled.div`
+  display: flex;
+  font-weight: bold;
+  align-items: center;
+  font-size: 1.3em;
+  text-transform: uppercase;
+  margin-bottom: 0.5em;
+`;
+
+const YearToDate = styled.label`
+  font-size: 0.75em;
+  text-transform: initial;
+  font-weight: normal;
+  margin-left: 1em;
+  input {
+    margin-right: 0.5em;
+  }
+`;
+
+const NetSpendAnalysis = () => {
+  return (
+    <Container>
+      <SectionTitle>
+        <span>Net Spend Analysis</span>
+        <Icon className="fas fa-info" info />
+        <YearToDate>
+          <input type="checkbox" />
+          Year to date
+        </YearToDate>
+      </SectionTitle>
+    </Container>
+  );
+};
+
+export default NetSpendAnalysis;
