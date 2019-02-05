e0662d018158a80de9476917b006dc79af5a33f5
Hanseul Shim <hanseulshim@gmail.com>
2019-02-05 15:12:54 -0500

    Update portal to handle its own route

--- src/components/Portal/index.js
+++ src/components/Portal/index.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import styled from 'styled-components';
+import Header from 'components/Header';
+import Sidebar from 'components/Sidebar';
+import UpcomingActions from 'components/Sidebar/UpcomingActions';
+import ActiveAlerts from 'components/Sidebar/ActiveAlerts';
 import { SectionTitle } from 'components/common/Typography';
 import ProgramSelect from './ProgramSelect';
 import BottomInfo from './BottomInfo';
 
 const Container = styled.div`
+  max-width: 1600px;
+  margin: auto;
+  display: flex;
+  position: relative;
+  height: 100%;
+`;
+
+const MainContainer = styled.div`
+  flex: 3;
+  padding: 0em 4em;
+`;
+
+const PortalContainer = styled.div`
   display: flex;
   flex-direction: column;
   margin-top: ${props => props.theme.verticalSpace};
@@ -12,9 +29,18 @@ const Container = styled.div`
 
 const Portal = () => (
   <Container>
-    <SectionTitle>products</SectionTitle>
-    <ProgramSelect />
-    <BottomInfo />
+    <Sidebar>
+      <UpcomingActions />
+      <ActiveAlerts />
+    </Sidebar>
+    <MainContainer>
+      <Header />
+      <PortalContainer>
+        <SectionTitle>products</SectionTitle>
+        <ProgramSelect />
+        <BottomInfo />
+      </PortalContainer>
+    </MainContainer>
   </Container>
 );
 
