183e0cb0471a552124e9873b703cf14a8d3ac94e
joneil@boostlabs.com <johnoneil@Johns-MacBook-Pro.local>
2019-04-12 11:42:07 -0400

    get in there you lil devil

--- src/components/TravelManager/Category/TeBreakdown/BarChartTe.js
+++ src/components/TravelManager/Category/TeBreakdown/BarChartTe.js
@@ -3,39 +3,23 @@ import styled from 'styled-components';
 
 const Container = styled.div`
   flex: 1;
+  display:flex;
+  flex-direction:column;
 `;
 
-const Bar = ({ accept, negotiating, rejected, target, solicited }) => {
-  const acceptWidth = { width: `${(accept / solicited) * 100}%` };
-  const negotatingWidth = { width: `${(negotiating / solicited) * 100}%` };
-  const rejectedWidth = { width: `${(rejected / solicited) * 100}%` };
-  const targetCalc = target / solicited >= 1 ? 113 : (target / solicited) * 100;
-  const targetWidth = { left: `${targetCalc}%` };
+const Bar = ({ personas}) => {
+//   const acceptWidth = { width: `${(accept / solicited) * 100}%` };
+//   const negotatingWidth = { width: `${(negotiating / solicited) * 100}%` };
+//   const rejectedWidth = { width: `${(rejected / solicited) * 100}%` };
+//   const targetCalc = target / solicited >= 1 ? 113 : (target / solicited) * 100;
+//   const targetWidth = { left: `${targetCalc}%` };
 
   return (
     <Container>
-      {accept ? (
-        <div style={{ ...styles.bar, ...styles.green, ...acceptWidth }}>
-          <span style={styles.label}>{accept}</span>
-        </div>
-      ) : null}
-      {negotiating ? (
-        <div style={{ ...styles.bar, ...styles.orange, ...negotatingWidth }}>
-          <span
-            style={
-              accept < 25 ? styles.topLabel : negotiating > target ? styles.label : styles.label
-            }
-          >
-            {negotiating}
-          </span>
-        </div>
-      ) : null}
-      {rejected ? (
-        <div style={{ ...styles.bar, ...styles.red, ...rejectedWidth }}>
-          <span style={styles.label}>{rejected}</span>
-        </div>
-      ) : null}
-      {target ? <span style={{ ...styles.target, ...targetWidth }}>{target} Targeted</span> : null}
+        {personas.map((persona,idx) => (
+            
+        ))}
+      
     </Container>
   );
 };
